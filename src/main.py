ccaa0df70fb904ae7a73b1e1ea38dc0c25a5431e
Azardani <azarce03@gmail.com>
2026-05-21 01:19:17 +0200

    plots

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv("car_price_dataset.csv", sep=";")
+from plots import price_distribution, price_vs_mileage
 
-df["Score"] = 0
+price_distribution(df)
+price_vs_mileage(df)
+
 
 df["Price"] = pd.to_numeric(df["Price"], errors="coerce")
 df = df.dropna(subset=["Price"])  
@@ -20,46 +23,53 @@ df["Year"] = pd.to_numeric(df["Year"], errors="coerce")
 df = df.dropna(subset=["Price", "Mileage", "Year"])
 
 
-Most_expensive = df[df["Price"] == df["Price"].max()]
+Most_expensive = df[df["Price"] == df["Price"].max()]   ## hittar den dyraste samt billigaste bilen
 Least_expensive = df[df["Price"] == df["Price"].min()]
 
-average_price = df["Price"].mean()
+average_price = df["Price"].mean() ## genomsnitliga priset av alla bilar
 
-most_miles = df[df["Mileage"] == df["Mileage"].max()]
+most_miles = df[df["Mileage"] == df["Mileage"].max()] ## mes och minst miltal
 least_miles = df[df["Mileage"] == df["Mileage"].min()]
 
-average_miles = df["Mileage"].mean()
+average_miles = df["Mileage"].mean() ## genosnitlliga miltal 
 
-newest_car = df[df["Year"] == df["Year"].max()]
+newest_car = df[df["Year"] == df["Year"].max()] ## nyaste och äldsta bilen
 oldest_car = df[df["Year"] == df["Year"].min()]
+average_age = df["Year"].mean() ## genomsnitts ålder på bilarna
 
-average_age = df["Year"].mean()
 
+cheap_cars = df[df["Price"] < average_price]  ## alla bilar som kostar mindre än genomsnittet räknas som billiga. 
+expensive_cars = df[df["Price"] > average_price]## alla som kostar mer är dyra
 
-cheap_cars = df[df["Price"] < average_price]
-expensive_cars = df[df["Price"] > average_price]
+high_mileage = df[df["Mileage"] > average_miles] ## högre samt lägre miltal än genomsnittet
+low_mileage = df[df["Mileage"] < average_miles] 
 
-high_mileage = df[df["Mileage"] > average_miles]
-low_mileage = df[df["Mileage"] < average_miles]
-
-old_cars = df[df["Year"] < average_age]
+old_cars = df[df["Year"] < average_age] ## kollar bilar äldre samt yngre än genomsnitt
 new_cars = df[df["Year"] > average_age]
 
 
-df["Score"] = 0
+df["Score"] = 0 ## säger att alla bilar har 0 poäng innan deras egenskaper jämförs
 
-df.loc[df["Mileage"] < average_miles, "Score"] += 1
-df.loc[df["Mileage"] > average_miles, "Score"] -= 1
-
+## poängsystem där man kan jämföra för och nackdelar med bilarna
+## en bil som är äldre än genomsnittet behöver inte vara en dålig bil 
+## den kan ha bra egenskaper som att den är billig 
+## eller har lågt miltal!
+df.loc[df["Mileage"] < average_miles, "Score"] += 1 
+df.loc[df["Mileage"] > average_miles, "Score"] -= 1  
 df.loc[df["Year"] > average_age, "Score"] += 1
 df.loc[df["Year"] < average_age, "Score"] -= 1
-
 df.loc[df["Price"] < average_price, "Score"] += 1
 df.loc[df["Price"] > average_price, "Score"] -= 1
 
 
+too_good_to_be_true_cars = df[df["Score"] == 3]
 Very_good_cars = df[df["Score"] >= 2]
 good_cars = df[df["Score"] == 1]
 neutral_cars = df[df["Score"] == 0]
-bad_cars = df[df["Score"] <= -1]
+bad_cars = df[df["Score"] == -1]
+Very_bad_cars = df[df["Score"] == -2]
+Horrible_cars = df[df["Score"] == -3]
+
+print("too_good_to_be_true_cars")
+print(too_good_to_be_true_cars.head(5))
 
--- src/plots.py
+++ src/plots.py
@@ -3,6 +3,21 @@ import pandas as pd
 df = pd.read_csv("car_price_dataset.csv", sep=";")
 
 
+
+plt.figure(figsize=(8,6))
+
+plt.hist(df["Price"], bins=20)
+
+plt.xlabel("Price")
+plt.ylabel("number of cars")
+plt.title("price distribution of cars")
+
+plt.tight_layout()
+
+plt.savefig("price_distribution.png", dpi=300, bbox_inches="tight")
+
+plt.show()
+
 plt.scatter(df["Mileage"], df["Price"])
 
 plt.xlabel("Mileage")
@@ -12,5 +27,31 @@ plt.title("Mileage vs Price")
 # sparar bilden
 plt.savefig("price_vs_mileage.png", dpi=300, bbox_inches="tight")
 
+import matplotlib.pyplot as plt
 
+brand_avg = df.groupby("Brand")["Price"].mean().sort_values(ascending=False).head(10)
+
+plt.figure(figsize=(10,6))
+
+bars = plt.bar(brand_avg.index, brand_avg.values)
+
+plt.xlabel("Brand")
+plt.ylabel("Average Price")
+plt.title("Top 10 Brands by Average Price")
+
+plt.xticks(rotation=45)
+
+# visar värden
+for bar in bars:
+    plt.text(
+        bar.get_x() + bar.get_width()/2,
+        bar.get_height(),
+        f"{bar.get_height():.0f}",
+        ha="center",
+        va="bottom"
+    )
+
+plt.savefig("Top_10_average", dpi=300, bbox_inches="tight")
+
+plt.tight_layout()
 plt.show()
